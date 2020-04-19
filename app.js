b932afb19c0bf293d084f994d5492407feab47af
Guilherme Morone Araujo <guimorone@gmail.com>
2020-04-19 17:42:07 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,81 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+//encryption
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+//mongoose will encrypt when we call "save()" and decrypt when we call "find" (ou seja, n precisa mudar o resto do código)
+const secret = process.env.SOME_LONG_UNGUESSABLE_STRING;
+userSchema.plugin(encrypt, {
+  secret: secret,
+  encryptedFields: ['password'] //só criptografar a senha
+});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+
+  newUser.save((err) => {
+    if(err) {
+      console.log(err);
+    } else{
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", (req, res) => {
+  User.findOne({email: req.body.username}, (err, foundUser) => {
+    if(err) {
+      console.log(err);
+    } else{
+      if(foundUser) {
+        if(foundUser.password === req.body.password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000!");
+});
